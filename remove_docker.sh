eac641f50ad5ed0c580031ce8fd7e58e7fc918b2
Marc Inizan <locstaw@gmail.com>
2021-05-03 19:21:23 +0100

    remove execution rights by default

--- remove_docker.sh
+++ remove_docker.sh
@@ -1,3 +1,7 @@
 sudo docker-compose down
 sudo docker rm $(docker container ls -aq)
 docker rmi -f $(docker image ls -aq)
+echo "List of Docker images:"
+docker image ls
+
+# docker rmi -f $(docker images "sel*/*" -q)
