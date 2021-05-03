83275bf996cfd751165812695afdb838c687b8c6
Marc Inizan <locstaw@gmail.com>
2021-05-03 16:21:27 +0100

    clean terminal output

--- start.sh
+++ start.sh
@@ -2,9 +2,6 @@
 sudo docker-compose down
 sudo docker-compose up -d
 
-# Wait for Airflow server to start
-sleep 3
-
 # Open AirFlow GUI
 python -m webbrowser http://localhost:8080/home
 
@@ -18,8 +15,14 @@ echo "##  Edge    VNC Server: 0.0.0.0:6901              ##"
 echo "##  Firefox VNC Server: 0.0.0.0:6902              ##"
 echo "##  Opera   VNC Server: 0.0.0.0:6903              ##"
 echo "####################################################"
-echo "\n"
-echo "Airflow Login    : 'airflow'"
-echo "Airflow Password : 'airflow'"
-echo "\n"
-echo "## Don't forget to stop the containers when you are done using stop.sh or remove_docker.sh !!"
+echo 
+echo "####################  LOGINS  ######################"
+echo "        Airflow Login    : 'airflow'"
+echo "        Airflow Password : 'airflow'"
+echo "####################################################"
+echo 
+echo "###################  IMPORTANT  ####################"
+echo "Don't forget to stop the containers when you are done using stop.sh or remove_docker.sh"
+echo "Wait a few seconds for the Airflow server to start, or refresh the browser"
+echo "More info here ==> https://github.com/locsta/Docker-Airflow-Selenium"
+echo "####################################################"
