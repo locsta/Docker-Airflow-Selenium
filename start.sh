41cc57ecc176e9c40f6316afd749de12e851fe1b
Marc Inizan <locstaw@gmail.com>
2021-05-03 15:46:53 +0100

    fixed link to airflow GUI

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
-# Shutdown containers if they are already runnning and run docker compose file
+# Shutdown containers if they are already runnning and run docker compose file in detached mode
 sudo docker-compose down
-sudo docker-compose up
+sudo docker-compose up -d
 
 # Open AirFlow GUI
-python -m webbrowser http://localhost:8080/
-
-echo "Selenium hub  => http://localhost:4444/wd/hub"
-echo "Selenium grid => http://localhost:4444/grid/console"
+python -m webbrowser http://localhost:8080/home
+echo "####################################################"
+echo "##  Selenium hub  => http://localhost:4444/       ##"
+echo "##                                                ##"
+echo "##               ~~VNC SERVERS~~                  ##"
+echo "##  Chrome  VNC Server: 0.0.0.0:6900              ##"
+echo "##  Edge    VNC Server: 0.0.0.0:6901              ##"
+echo "##  Firefox VNC Server: 0.0.0.0:6902              ##"
+echo "##  Opera   VNC Server: 0.0.0.0:6903              ##"
+echo "####################################################"
